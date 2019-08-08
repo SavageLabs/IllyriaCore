9b5cc0b57da6991d1aeb2d463f0e331ea428c0ef
theone1000 <unconfigured@null.spigotmc.org>
2019-08-08 02:47:53 -0600

    changed most (if not, all) enum comparing to == and some other things

--- src/main/java/me/ryder/savagecore/utils/Methods.java
+++ src/main/java/me/ryder/savagecore/utils/Methods.java
@@ -3,7 +3,7 @@ package me.ryder.savagecore.utils;
 import org.bukkit.ChatColor;
 
 public class Methods {
-    
+
     public static String pl(String msg) {
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
